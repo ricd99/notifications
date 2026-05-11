9a10e625554f8b9f5af4e4f0ab70c70141f3aae7
ricd99 <ryanhuang6391@gmail.com>
2026-05-11 13:41:11 -0700

    job -> listing

--- src/notifier.py
+++ src/notifier.py
@@ -1,3 +1,17 @@
 """
 Notification functions
 """
+
+import requests
+
+def send_notification(title:str, message: str, topic: str, priority: int = 3) -> None:
+    response = requests.post(
+        url=f"https://ntfy.sh/{topic}",
+        data=message.encode("utf-8"),
+        headers={
+            "Title": title,
+            "Priority": str(priority),
+            "Tags": "briefcase",
+        },
+    )
+    response.raise_for_status()
